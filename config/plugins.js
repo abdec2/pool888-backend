cfb05b3dff40048909b03b131c49cfc44b2ad2a1
abdec2 <abdec2@hotmail.com>
2022-09-17 21:40:33 +0300

    updated

---
+++ config/plugins.js
@@ -0,0 +1,21 @@
+module.exports = ({ env }) => ({
+    // ...
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: 'smtp.gmail.com',
+          port:  587,
+          auth: {
+            user: env('SMTP_USERNAME'),
+            pass: env('SMTP_PASSWORD'),
+          },
+          // ... any custom nodemailer options
+        },
+        settings: {
+          defaultFrom: env('SMTP_USERNAME'),
+          defaultReplyTo: env('SMTP_USERNAME'),
+        },
+      },
+    },    // ...
+  });
