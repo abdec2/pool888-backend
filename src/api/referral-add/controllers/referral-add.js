dd731a563b26d279d0ae5cd070c0f76db0334a7b
abdec2 <abdec2@hotmail.com>
2022-10-11 07:22:47 +0300

    Update referral-add.js

--- src/api/referral-add/controllers/referral-add.js
+++ src/api/referral-add/controllers/referral-add.js
@@ -7,31 +7,49 @@
 module.exports = {
   addref: async (ctx, next) => {
     try {
-      const users = await strapi.entityService.findMany('api::wallet.wallet', {
-        sort: {id: 'ASC'}
-      })
-      users.map(async item => {
-        let connected_wallet =  item; 
-        let current_package_level = 0;
-        let current_wallet = connected_wallet
+      const {id} = ctx.request.query;
+      const item = await strapi.entityService.findOne('api::wallet.wallet', id, {})
+      let connected_wallet = item;
+      let current_package_level = 0;
+      let current_wallet = connected_wallet
 
-        for(let level = 1; level <= 4; level++) {
-          let parent_wallet = await strapi.service('api::referral-add.referral-add').parentOf(current_wallet);
-          if(parent_wallet){ 
-            if(parent_wallet.package !== null && parent_wallet.package.level > current_package_level) {
-                await strapi.service('api::referral-add.referral-add').createReferral(parent_wallet, connected_wallet, level);
-            }
+      for (let level = 1; level <= 4; level++) {
+        let parent_wallet = await strapi.service('api::referral-add.referral-add').parentOf(current_wallet);
+        if (parent_wallet) {
+          if (parent_wallet.package !== null && parent_wallet.package.level > current_package_level) {
+            await strapi.service('api::referral-add.referral-add').createReferral(parent_wallet, connected_wallet, level);
           }
-          else break;
-          current_wallet = parent_wallet;
-          current_package_level++;
         }
-      })
-       ctx.send(JSON.stringify(users))
+        else break;
+        current_wallet = parent_wallet;
+        current_package_level++;
+      }
+
+      // const users = await strapi.entityService.findMany('api::wallet.wallet', {
+      //   sort: {id: 'ASC'}
+      // })
+      // users.map(async item => {
+      //   let connected_wallet =  item; 
+      //   let current_package_level = 0;
+      //   let current_wallet = connected_wallet
+
+      //   for(let level = 1; level <= 4; level++) {
+      //     let parent_wallet = await strapi.service('api::referral-add.referral-add').parentOf(current_wallet);
+      //     if(parent_wallet){ 
+      //       if(parent_wallet.package !== null && parent_wallet.package.level > current_package_level) {
+      //           await strapi.service('api::referral-add.referral-add').createReferral(parent_wallet, connected_wallet, level);
+      //       }
+      //     }
+      //     else break;
+      //     current_wallet = parent_wallet;
+      //     current_package_level++;
+      //   }
+      // })
+      ctx.send(JSON.stringify(item))
       //ctx.body = 'ok';
     } catch (err) {
       ctx.body = err;
     }
-    return 
+    return
   }
 };
