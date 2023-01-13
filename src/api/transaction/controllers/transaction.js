88152d79cfa9cac7081ec9c88f5b9880ff1a61d5
Tayyaba Khalil <tayyaba.khalil@gmail.com>
2023-01-13 19:15:19 +0300

    Transactions Retrieval API
    Transactions Retrieval API

--- src/api/transaction/controllers/transaction.js
+++ src/api/transaction/controllers/transaction.js
@@ -16,23 +16,71 @@ module.exports = createCoreController('api::transaction.transaction', ({ strapi 
                 populate: {
                   wallet: {
                     populate: {
-                      package: true, users_permissions_user: true
+                        package: true, users_permissions_user: true, pool:true
                     }
                   }, 
                   referral: {
                     populate: {
                       parent_wallet: {
-                       package: true, users_permissions_user:true
+                       package: true, users_permissions_user:true, pool:true
                       },                       
                       child_wallet: {
-                        package: true, users_permissions_user:true
+                        package: true, users_permissions_user:true, pool:true
                        }
                     }
                   }
                 }, 
                 orderBy: 'createdAt'
             });
-            return transactions;
+
+            const transactionWDetail = Promise.all(transactions.map(async item => {
+
+              let description = null;
+              let amount = null
+
+              if (item.type === 'package') {
+
+                description = `Acquisition of ${item.wallet.package.name} package`
+                amount = `${item.amount} ${item.wallet.pool.currency}`
+                            
+              } else if (item.type === 'feededucted') {  
+
+                description = `5% fee deduction`
+                amount = `${item.amount} ${item.wallet.pool.currency}`
+
+              } else if (item.type === 'staking') {
+
+                description = `60% of package value stake 888 tokens`
+                amount = `${item.amount} ${item.wallet.pool.currency}`
+
+              } else if (item.type === 'tokenspurchase') {
+
+                description = `40% of package value buys 888 tokens`
+                amount = `${item.amount} ${item.wallet.pool.currency}`
+
+              } else if (item.type === 'harvest') {
+
+                description = `888 tokens harvested/claimed` 
+                amount = `${item.amount} 888`          
+
+              } else if (item.type === 'withdrawal') {
+
+                description = `Withdrawal of Staked Amount`
+                amount = `${item.amount} ${item.wallet.pool.currency}`  
+
+              } else if (item.type === 'commission'){
+
+                description = `Received commission from ${item.referal.child_wallet.wallet_id}`  
+                amount = `${item.amount} 888`  
+              }
+                return {
+                  "TRANSACTION TIME" : item.createdAt,
+                  "DESCRIPTION": description,
+                  "AMOUNT": amount
+                }
+              }))                                   
+
+            return transactionWDetail;
 
         } catch(e) {
             console.log(e)
@@ -48,23 +96,72 @@ module.exports = createCoreController('api::transaction.transaction', ({ strapi 
                 populate: {
                   wallet: {
                     populate: {
-                      package: true, users_permissions_user: true
+                      package: true, users_permissions_user: true, pool:true
                     }
                   }, 
                   referral: {
                     populate: {
                       parent_wallet: {
-                       package: true, users_permissions_user:true
+                       package: true, users_permissions_user:true, pool:true
                       },                       
                       child_wallet: {
-                        package: true, users_permissions_user:true
+                        package: true, users_permissions_user:true, pool:true
                        }
                     }
                   }
                 }, 
                 orderBy: 'createdAt'
             });
-            return transactions;
+
+            const transactionWDetail = Promise.all(transactions.map(async item => {
+
+              let description = null;
+              let amount = null
+
+              if (item.type === 'package') {
+
+                description = `Acquisition of ${item.wallet.package.name} package`
+                amount = `${item.amount} ${item.wallet.pool.currency}`
+                            
+              } else if (item.type === 'feededucted') {  
+
+                description = `5% fee deduction`
+                amount = `${item.amount} ${item.wallet.pool.currency}`
+
+              } else if (item.type === 'staking') {
+
+                description = `60% of package value stake 888 tokens`
+                amount = `${item.amount} ${item.wallet.pool.currency}`
+
+              } else if (item.type === 'tokenspurchase') {
+
+                description = `40% of package value buys 888 tokens`
+                amount = `${item.amount} ${item.wallet.pool.currency}`
+
+              } else if (item.type === 'harvest') {
+
+                description = `888 tokens harvested/claimed` 
+                amount = `${item.amount} 888`          
+
+              } else if (item.type === 'withdrawal') {
+
+                description = `Withdrawal of Staked Amount`
+                amount = `${item.amount} ${item.wallet.pool.currency}`  
+
+              } else if (item.type === 'commission'){
+
+                description = `Received commission from ${item.referal.child_wallet.wallet_id}`  
+                amount = `${item.amount} 888`  
+              }
+                return {
+                  "TRANSACTION TIME" : item.createdAt,
+                  "DESCRIPTION": description,
+                  "AMOUNT": amount
+                }                   
+
+              }))  
+                          
+            return transactionWDetail;
 
         } catch(e) {
             console.log(e)
